7f2c60544e8b559b1b5424ee50c2a3a1bef179d8
Aquiles Gomez <107149224+Aquiles0603@users.noreply.github.com>
2022-12-14 20:02:02 -0300

    Add files via upload

---
+++ VS FINAL/js/funciones.js
@@ -0,0 +1,8 @@
+function Color1(){
+    var ModoOscuro2=document.getElementById("Oscuro2");
+    ModoOscuro2.style.background="white";
+}
+function Color2(){
+    var ModoClaro1=document.getElementById("Oscuro2");
+    ModoClaro1.style.background="grey";
+}
