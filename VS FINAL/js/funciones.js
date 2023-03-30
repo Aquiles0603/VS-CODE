22772005b6b358e0e45e62d3a188ecc97d177ab1
Aquiles0603 <maximogomez370@gmail.com>
2023-03-30 15:34:02 -0300

    commit

--- VS FINAL/js/funciones.js
+++ VS FINAL/js/funciones.js
@@ -6,3 +6,23 @@ function Color2(){
     var ModoClaro1=document.getElementById("Oscuro2");
     ModoClaro1.style.background="black";
 }
+
+const allStars = document.querySelectorAll('.star');
+let current_rating = document.querySelector('.current-rating');
+allStars.forEach((star, i) =>{
+    star.onclick = function(){
+        let current_star_level = i + 1;
+        current_rating.innerText = `${current_star_level}of 5`;
+
+        allStars.forEach((star,j)=>{
+            if( current_star_level >= j+1)
+            {
+                star.innerHTML = '&#9733';
+                
+            }else{
+                star.innerHTML = '&#9734';
+            }
+            5 >= 1,2,3,4,5
+        })
+    }
+})
