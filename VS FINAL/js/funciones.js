b8426528f56e43683e8d4bb9294d6cfbdbdb81e4
Aquiles0603 <maximogomez370@gmail.com>
2023-04-03 19:53:14 -0300

    commit

--- VS FINAL/js/funciones.js
+++ VS FINAL/js/funciones.js
@@ -26,3 +26,10 @@ allStars.forEach((star, i) =>{
         })
     }
 })
+
+const acordeon = document.getElementsByClassName('contentBx');
+for (i = 0; i<acordeon.length; i++){
+    acordeon[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+}
