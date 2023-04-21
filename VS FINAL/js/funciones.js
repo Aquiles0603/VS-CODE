4a427ed9bc6489c0d607efe9cd54f486d1abb89a
Aquiles0603 <maximogomez370@gmail.com>
2023-04-21 11:51:56 -0300

    commit

--- VS FINAL/js/funciones.js
+++ VS FINAL/js/funciones.js
@@ -51,8 +51,13 @@ function preguntarNombreyApellido() {
 	alert (`Bienvenido a VoleyWeb ${nombre} ${apellido}`);
 	}
 
- 
-function cambiarModo() { 
-		var cuerpoweb = document.body; 
-		cuerpoweb.classList.toggle("oscuro");
+	const fulImgBox = document.getElementById("fulImgBox"),
+	fulImg = document.getElementById("fulImg");
+	
+	function openFulImg(reference){
+		fulImgBox.style.display = "flex";
+		fulImg.src = reference
+	}
+	function closeImg(){
+		fulImgBox.style.display = "none";
 	}
